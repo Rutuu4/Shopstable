fed7489136e4b3b2a2ade7868055b45363613355
bstory <bstory32@gmail.com>
2022-04-27 07:57:23 -0500

    moved centeral domains config into env file

--- config/tenancy.php
+++ config/tenancy.php
@@ -19,9 +19,9 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        // '127.0.0.1',
+         //'127.0.0.1',
         // 'localhost',
-        'master.net',
+        env('DOMAIN'),
     ],
 
     /**
