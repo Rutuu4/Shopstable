1048fb03a30ae392ff392bc25b33e0f479b4ceeb
Arth Raval <connect.arth.raval@gmail.com>
2022-05-12 14:31:40 +0530

    Order Development Start

---
+++ database/migrations/tenant/2022_05_12_082942_create_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('user_name');
+            $table->integer('card_no');
+            $table->date('exapiration_date');
+            $table->integer('cvc');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->integer('postal_code');
+            $table->string('payment_mode');
+            $table->float('sub_total');
+            $table->float('discount')->nullable();
+            $table->float('tax');
+            $table->float('shipping');
+            $table->float('total');
+            $table->string('order_status');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
