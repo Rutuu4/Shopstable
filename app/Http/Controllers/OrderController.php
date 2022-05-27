cc19e15224dacc4e1c1d1835f63d03704cddcadf
Arth Raval <connect.arth.raval@gmail.com>
2022-05-27 19:21:20 +0530

    Order Items API

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Menubuilder;
 use App\Models\Order;
 use App\Models\Orders_items;
+use App\Models\Product;
+use App\Models\Purchase_items;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 
@@ -20,12 +24,16 @@ class OrderController extends Controller
     public function index()
     {
         $order = Order::get();
+        $purchase_items = Purchase_items::leftjoin('product', 'product.id', '=', 'purchase_items.product_id')
+            ->leftjoin('product_image', 'product_image.product_id', '=', 'product.id')
+            ->select('product.id as product_id', 'product.title', 'product_image.imageName', 'purchase_items.price', 'purchase_items.quantity', 'purchase_items.sub_total')
+            ->get();
         $navbar = Menubuilder::orderBy('nav_item_order', 'ASC')->get();
         if (empty($order)) {
             return view('orders.order');
         }
         if (!empty($order)) {
-            return view('order.order', ['order' => $order,'navbar' => $navbar]);
+            return view('order.order', ['order' => $order, 'navbar' => $navbar, 'datas' => $purchase_items]);
         }
     }
 
@@ -47,26 +55,40 @@ class OrderController extends Controller
     public function store(Request $request)
     {
         try {
-            $order = Order::insert([
-                'email' => $request->email,
-                'user_name' => $request->user_name,
-                'card_no' => $request->card_no,
-                'exapiration_date' => $request->exapiration_date,
-                'cvc' => $request->cvc,
-                'address' => $request->address,
-                'city' => $request->city,
-                'state' => $request->state,
-                'postal_code' => $request->postal_code,
-                'payment_mode' => $request->payment_mode,
-                'sub_total' => $request->sub_total,
-                'discount' => $request->discount,
-                'tax' => $request->tax,
-                'shipping' => $request->shipping,
-                'total' => $request->total,
-                'order_status' => $request->order_status,
+            $order = new Order();
+            $order->email = $request->email;
+            $order->user_name = $request->fisrt_name . '' . $request->last_name;
+            $order->card_no = $request->card_no;
+            $order->exapiration_date
+                = Carbon::parse($request->exapiration_date)->format('Y-m-d');
+            // $order->exapiration_date= $request->exapiration_date;
+            $order->cvc = $request->cvc;
+            $order->address = $request->address;
+            $order->city = $request->city;
+            $order->state = $request->state;
+            $order->postal_code = $request->postal_code;
+            $order->country = $request->country;
+            $order->total = $request->order_total;
+            $order->payment_mode = $request->payment_mode;
+            $order->save();
+            $order_id = $order->id;
 
+            $category_id =DB::table('product_category')->where('product_id', $request->product_id)->first();
+            // dd($category_id->category_id);
+            $order_items = new Orders_items();
+            $order_items->order_id = $order_id;
+            $order_items->product_id = $request->product_id;
+            $order_items->category_id = $category_id->category_id;
+            $order_items->quantity = $request->quantity;
+            $order_items->price = $request->price;
+            $order_items->sub_total = $request->sub_total;
+            $order_items->save();
+
+
+            return redirect('shipping')->with([
+                'success' => "uploaded", 'order' => $request->$order
             ]);
-            return response()->json(['success' => "uploaded", 'order' => $request->$order]);
+            // return response()->json(['success' => "uploaded", 'order' => $request->$order]);
         } catch (Exception $e) {
             Log::error($e->getMessage());
             return response()->json(['error' => $e->getMessage() . ' ' . $e->getLine()]);
@@ -84,6 +106,7 @@ class OrderController extends Controller
         //
         try {
             $order = Order::where('id', $id)->first();
+
             $items = Orders_items::where('order_id', $id)->get();
 
             if (empty($order)) {
