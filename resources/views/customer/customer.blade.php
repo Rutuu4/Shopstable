7c952cb4753256156c4c4408f2262048887fbef2
Arth Raval <connect.arth.raval@gmail.com>
2022-06-08 19:09:31 +0530

    Admin UI

---
+++ resources/views/customer/customer.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    <script>
+    $(document).ready(function() {
+    $('#myTable').DataTable();
+    } );
+    </script>
+</head>
+
+<body>
+    <div class="container pt-5">
+        <table id="myTable" class="table table-bordered table-striped" style="width:100%">
+            <thead>
+                <tr>
+                    <th> Id </th>
+                    <th> Name </th>
+                    <th> Address </th>
+                    <th> No of Orders</th>
+                    <th> Total </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($customers as $customer)
+                <tr>
+                    <td>{{$customer->id}}</td>
+                    <td>{{$customer->name}}</td>
+                    <td>{{$customer->address}}</td>
+                    <td>{{$customer->no_of_orders}}</td>
+                    <td>{{$customer->total}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
